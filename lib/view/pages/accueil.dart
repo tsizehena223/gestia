658993b5542f4d0646b5d73e50b5dd2f30e14908
tsizehena <tsizehena223@gmail.com>
2024-06-30 13:21:31 +0300

    update: remove default balance

--- lib/view/pages/accueil.dart
+++ lib/view/pages/accueil.dart
@@ -12,10 +12,8 @@ class Accueil extends StatefulWidget {
 
 class _AccueilState extends State<Accueil> {
   bool _isInputNameValid = false;
-  bool _isInputNumberValid = false;
 
   TextEditingController userNameController = TextEditingController();
-  TextEditingController userBalanceController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -78,52 +76,14 @@ class _AccueilState extends State<Accueil> {
                 },
               ),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 30),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                controller: userBalanceController,
-                style: const TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).primaryColorDark,
-                    ),
-                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                  ),
-                  fillColor: Colors.white,
-                  labelText: "Type your current balance",
-                  labelStyle: TextStyle(color: Theme.of(context).focusColor),
-                  suffix: const Text("Ar", style: TextStyle(color: Colors.white),),
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                  ),
-                ),
-                onChanged: (String? value) => {
-                  if (value == null || value == "" || int.tryParse(value) == null) {
-                    setState(
-                      () {
-                      _isInputNumberValid = false;
-                      }
-                    )
-                  } else {
-                    setState(() {
-                      _isInputNumberValid = true;
-                    })
-                  },
-                },
-              ),
-            ),
-            // End Input
+           // End Input
             Visibility(
-              visible: (_isInputNameValid && _isInputNumberValid) ? true : false,
+              visible: (_isInputNameValid) ? true : false,
               child: Container(
-                margin: const EdgeInsets.only(top: 50),
+                margin: const EdgeInsets.only(top: 30),
                 child: ElevatedButton(
-                  onPressed: !(_isInputNameValid && _isInputNumberValid) ? null : () async {
+                  onPressed: !(_isInputNameValid) ? null : () async {
                     await SharedPreferencesUtil.storeUserName(userNameController.text);
-                    await SharedPreferencesUtil.storeBalance(int.parse(userBalanceController.text));
-        
                     Navigator.push(
                       // ignore: use_build_context_synchronously
                       context,
@@ -132,10 +92,10 @@ class _AccueilState extends State<Accueil> {
                   },
                   style: ElevatedButton.styleFrom(
                     padding: const EdgeInsets.symmetric(
-                      vertical: 10,
+                      vertical: 15,
                       horizontal: 70,
                     ),
-                    backgroundColor: Theme.of(context).primaryColorDark,
+                    backgroundColor: Theme.of(context).primaryColorDark.withOpacity(.5),
                   ),
                   child: const Text(
                     "Start",
