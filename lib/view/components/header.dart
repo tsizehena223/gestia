d0aba083885d8cb7c8c137b2a2dcdb80da9fd1ce
tsizehena223 <tsizehena223@gmail.com>
2024-10-21 20:59:43 +0300

    feat : update profile name

--- lib/view/components/header.dart
+++ lib/view/components/header.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:gestia/main.dart';
 import 'package:gestia/utils/shared_preferences_util.dart';
 
-class Header extends StatelessWidget {
+class Header extends StatefulWidget {
   const Header({
     super.key,
   });
+
+  @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  void _reloadApp(BuildContext context) {
+    Navigator.of(context).popUntil((route) => route.isFirst);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const MyApp(),
+      ),
+    );
+  }
+
+  Future<void> _updateProfile(BuildContext context) async {
+    TextEditingController nameController = TextEditingController();
+
+    return await showDialog(
+      // ignore: use_build_context_synchronously
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Update your name',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).primaryColorDark,
+            ),
+          ),
+          content: Form(
+            child: TextField(
+              keyboardType: TextInputType.text,
+              controller: nameController,
+              decoration: inputDecoration(context, 'Username'),
+              style: TextStyle(color: Theme.of(context).disabledColor),
+            )
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                  await SharedPreferencesUtil.storeUserName(nameController.text);
+                  // ignore: use_build_context_synchronously
+                  Navigator.of(context).pop(true);
+                  // ignore: use_build_context_synchronously
+                  _reloadApp(context);
+              },
+              child: Text('Confirm', style: TextStyle(color: Theme.of(context).primaryColorDark),),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,12 +97,38 @@ class Header extends StatelessWidget {
               "Welcome to GestIA",
               style: TextStyle(color: Theme.of(context).focusColor, fontSize: 12),
             ),
-            trailing: CircleAvatar(
-              backgroundColor: Theme.of(context).primaryColor,
-              child: Icon(Icons.person, color: Theme.of(context).primaryColorDark,),
+            trailing: GestureDetector(
+              onTap: () => _updateProfile(context),
+              child: CircleAvatar(
+                backgroundColor: Theme.of(context).primaryColor,
+                child: Icon(Icons.person, color: Theme.of(context).primaryColorDark,),
+              ),
             ),
           );
         }
+      ),
+    );
+  }
+
+  InputDecoration inputDecoration(BuildContext context, String label) {
+    return InputDecoration(
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Theme.of(context).primaryColor,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      focusColor: Colors.white,
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Theme.of(context).focusColor,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      labelText: label,
+      labelStyle: TextStyle(color: Theme.of(context).focusColor),
+      border: const OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
     );
   }
