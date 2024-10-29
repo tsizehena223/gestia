4c17be09ffbf14faeb2ed12076fd48e88c0fd459
tsizehena223 <tsizehena223@gmail.com>
2024-10-29 19:50:52 +0300

    last update ui

--- lib/view/components/list_transaction_history_widget.dart
+++ lib/view/components/list_transaction_history_widget.dart
@@ -67,40 +67,67 @@ class _ListTransactionHistoryWidgetState
                   return Card(
                     margin: const EdgeInsets.all(8),
                     color: isCurrentMonth
-                        ? Theme.of(context).primaryColorDark.withOpacity(.8)
-                        : Theme.of(context).disabledColor.withOpacity(.1),
-                    child: ListTile(
-                      title: Center(
-                        child: Text(
-                          isCurrentMonth
-                              ? "Current month"
-                              : "${transaction.year} : ${transaction.month}",
-                          style: TextStyle(
-                            color: isCurrentMonth
-                                ? Theme.of(context).primaryColorLight
-                                : Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                      subtitle: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        ? Theme.of(context).primaryColorDark.withOpacity(.7)
+                        : Theme.of(context).disabledColor.withOpacity(.2),
+                    elevation: 8,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      height: 150,
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            'Expense: ${transaction.expense} Ar',
-                            style: TextStyle(
-                              color: isCurrentMonth
-                                  ? Theme.of(context).disabledColor.withOpacity(.6)
-                                  : Theme.of(context).focusColor,
+                          Center(
+                            child: Container(
+                              width: 60.0,
+                              height: 60.0,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2.0,
+                                ),
+                              ),
+                              child: CircleAvatar(
+                                radius: 30, 
+                                backgroundColor: Theme.of(context).primaryColor.withOpacity(0),
+                                child: Icon(Icons.money, color: Theme.of(context).primaryColor,),
+                              ),
                             ),
                           ),
+                          const SizedBox(height: 8),
                           Text(
-                            'Income: ${transaction.income} Ar',
+                            isCurrentMonth ? "Current Month" : "${transaction.year} : ${transaction.month}",
                             style: TextStyle(
+                              fontSize: 20,
                               color: isCurrentMonth
-                                  ? Theme.of(context).primaryColor.withOpacity(.9)
-                                  : Theme.of(context).primaryColorDark,
+                                  ? Theme.of(context).primaryColorLight
+                                  : Theme.of(context).primaryColor,
                             ),
-                          ), 
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Income: ${transaction.income} Ar',
+                                style: TextStyle(
+                                  color: isCurrentMonth
+                                      ? Theme.of(context).primaryColor.withOpacity(.9)
+                                      : Theme.of(context).primaryColorDark,
+                                ),
+                              ),
+                              Text(
+                                'Expense: ${transaction.expense} Ar',
+                                style: TextStyle(
+                                  color: isCurrentMonth
+                                      ? Theme.of(context).disabledColor
+                                      : Theme.of(context).primaryColor.withOpacity(.7),
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     ),
